8457e127e6c6b577c640d3f6b084f491baf75b9c
Matheus Franciscone <mathfranciscone@gmail.com>
2020-10-15 01:10:30 -0300

    End Day #1

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,38 @@
-import { stringify } from 'querystring';
 import React from 'react';
+
+import './styles/global.css';
+
+import Routes from './routes'
 
 // JSX - JavaScript XML (Forma de Incluiir HTML em JS)
 
 // *CORE DO REACT, COMPONENTES E PROPRIEDADES*
-interface TitleProps {
-  text: string;
-}
+// interface TitleProps {
+//   text: string;
+// }
 
 
-function Title(props: TitleProps){
-  return (
-    <h1>{props.text}</h1>
-  )
-}
+// function Title(props: TitleProps){
+//   return (
+//     <h1>{props.text}</h1>
+//   )
+// }
 
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <Title text = "Titulo 1" />
+//       <Title text = "Titulo 2" />
+//       <Title text = "Titulo 3" />
+//       <Title text = "Titulo 4" />
+//     </div>
+//   );
+// }
 
 function App() {
   return (
-    <div className="App">
-      <Title text = "Titulo 1" />
-      <Title text = "Titulo 2" />
-      <Title text = "Titulo 3" />
-      <Title text = "Titulo 4" />
-    </div>
+    <Routes />
   );
 }
 
